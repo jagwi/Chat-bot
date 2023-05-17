79037bc66327ce6532381700989d5cdcbc74d499
jagwi <128573004+jagwi@users.noreply.github.com>
2023-05-17 22:00:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def is_admins(chat_id: int):
 
 @bot.on_message(filters.command("sta"))
 async def start(client, message):
-        await message.reply_text("Hi! My name is Ishi. I'm an Artificial Intelligence\n /chatbot - [on|off]")
+        await message.reply_text("Hii")
 
 
 @bot.on_message(
